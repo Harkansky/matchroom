ee93cb7adb61bc336d45d98c7839e865a029c334
Adrien Quacchia <777adrien@gmail.com>
2025-04-23 13:49:03 +0200

    setup app avec front: symfony serve + npm run dev-server

---
+++ assets/src/components/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-purple-500 text-3xl">
+          Click on the Vite and React logos to learn more
+        </p>
+        <button className="btn">Default</button>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
