45eb91da8aa7c0e483fb7cf86445c1310b918de6
Adrien Quacchia <777adrien@gmail.com>
2025-04-23 16:15:50 +0200

    login / register + router

---
+++ assets/src/components/Input.jsx
@@ -0,0 +1,10 @@
+const Input = () => {
+  return (
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend text-gray-500">Email</legend>
+      <input type="text" className="input rounded-lg" placeholder="Type here" />
+    </fieldset>
+  );
+};
+
+export default Input;
--- assets/src/main.jsx
+++ assets/src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
+import App from "./components/App";
 import "./assets/main.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="container m-auto">
-      <App />
+      <HashRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </HashRouter>
     </div>
   </StrictMode>
 );
