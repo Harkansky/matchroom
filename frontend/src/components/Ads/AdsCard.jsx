f6692338846c06f2e4d585ea202d6b0f3f81c29e
Adrien Quacchia <777adrien@gmail.com>
2025-04-24 14:13:42 +0200

    add home page elements

--- frontend/src/components/Ads/AdsCard.jsx
+++ frontend/src/components/Ads/AdsCard.jsx
@@ -1,13 +1,14 @@
 const AdsCard = () => {
   return (
     <div>
-      <div className="w-full bg-gray-300 h-[200px] rounded-lg"></div>
+      <div className="w-full bg-[url(/img/ads2.jpg)] bg-cover  h-[200px] rounded-lg"></div>
       <h3>Bel maison 4 pieces, France</h3>
       <div className="flex justify-between items-center">
         <span className="font-bold">Hotel Neyer</span>
         <span>4.5</span>
       </div>
       <span>Du 4 mai - 9 mai</span>
+      <span>180€/nuit</span>
     </div>
   );
 };
